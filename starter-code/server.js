f38d6a2ff7e85e360e1976d627f07bb44332c7f6
yanil3500 <yanil3500@gmail.com>
2017-02-14 16:27:53 -0800

    Three of five todos done

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,14 +1,16 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE: Initialize your project using NPM to create and populate a package.json file
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
+const express = require('express');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
-
+// DONE: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
 app.get('*', function(request, response) {
   // TODO: Using the response object, send the index.html file back to the user
 });
