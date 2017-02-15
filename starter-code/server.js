b7ecea03b6b0083ef586e49260713def95db8bf0
Kyle Hillman <khillman84@gmail.com>
2017-02-14 16:41:56 -0800

    finished last three TODOs

--- starter-code/server.js
+++ starter-code/server.js
@@ -11,12 +11,16 @@ const PORT = process.env.PORT || 3000;
 
 // DONE: Include all of the static resources as an argument to app.use()
 app.use(express.static('./public'));
-app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
+app.get('/index.html', function(request, response) {
+  // DONE: Using the response object, send the index.html file back to the user
+  response.sendFile('./public/index.html', {root:'.'});
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
+app.get('/new.html', function(request, response){
+  response.sendFile('./public/new.html', {root: '.'});
+});
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // DONE: Log to the console a message that lets you know which port your server has started on
+  console.log('We are on port:3000');
 });
